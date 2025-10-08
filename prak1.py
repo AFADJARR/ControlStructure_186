caee05f01a60948bf68fef2403c5c81b33900d68
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-10-08 15:05:40 +0700

    first commit

---
+++ prak1.py
@@ -0,0 +1,12 @@
+percentage = float (input("Masukan presbetase nilai siswa: "))
+
+if percentage >= 90:
+        print("Excelent performance")
+elif percentage >= 80:
+        print("Very Good performance")
+elif percentage >=70:
+        print("Good performance")
+elif percentage >=60:
+        print("Average performance")
+else:
+    print("Needs Improvement")
